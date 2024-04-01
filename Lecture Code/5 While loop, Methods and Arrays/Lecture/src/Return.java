b9522322a6525d08978f5f275537eee628703d69
way2masoom <way2masoom@gmail.com>
2024-04-01 23:59:49 +0530

    Return method

---
+++ Lecture Code/5 While loop, Methods and Arrays/Lecture/src/Return.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Return {
+    public static void main(String[] args) {
+    //calling greet method
+        greet();
+    // calling Number read method
+        int first=readNumber();     // calling method
+        int second=readNumber();    // calling method
+
+    //Adding two numbers
+        int sum=first+second;
+        System.out.println("The sum of two number is = "+sum);
+    }
+
+    //creating a greet method
+    public static void greet(){
+        System.out.println("Welcome to Calculator \n");
+    }
+    //creating a method to read numebrs from user
+    public static int readNumber() {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int number=input.nextInt();
+        return number;
+    }
+}
