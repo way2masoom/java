374b409d0fbb1b46fc8c4d21ef4464de6fa112bd
way2masoom <way2masoom@gmail.com>
2024-04-02 00:36:53 +0530

    Parameter is completed with example

--- Lecture Code/5 While loop, Methods and Arrays/Lecture/src/Parameter.java
+++ Lecture Code/5 While loop, Methods and Arrays/Lecture/src/Parameter.java
@@ -1,5 +1,20 @@
 public class Parameter {
     public static void main(String[] args) {
         System.out.println("Example of Parameter Method \n");
+
+        int sum = sumTwoNumber(4,7); // calling method and also passing values
+
+        System.out.println("Sum of two number is = "+sum);
+        System.out.println(sumTwoNumber(14,71));
+
+    }
+
+    //    creating a method with Parameter
+    public static int sumTwoNumber(int first, int second){
+        System.out.println("First number is = "+first);
+        System.out.println("Second number is = "+second);
+        int sum=first+second;
+        return sum;
     }
 }
+
