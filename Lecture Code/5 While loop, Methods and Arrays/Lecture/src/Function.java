0ff1948ada4d497d75810cfafd96e5820a3315e5
way2masoom <way2masoom@gmail.com>
2024-04-01 23:37:41 +0530

    Played with Methon and also used loop

--- Lecture Code/5 While loop, Methods and Arrays/Lecture/src/Function.java
+++ Lecture Code/5 While loop, Methods and Arrays/Lecture/src/Function.java
@@ -1,5 +1,40 @@
 public class Function {
     public static void main(String[] args) {
         System.out.println("Learning Function/ Method");
+        System.out.println("\n");
+
+        greetUser(); //Calling method
+
+        printFirstpattern();
+        System.out.println("\n");
+        printSecondpattern();
+    }
+    //creating a method
+    public static void greetUser(){
+        System.out.println("Good Morning From MD");
+    }
+
+    public static void printFirstpattern()
+    {
+        int rows=0;
+        while(rows<5){
+            System.out.print("*");
+            int i=0;
+            while (i<rows){
+                System.out.print(" *");
+                i++;
+            }
+            System.out.println();
+            rows++;
+        }
+    }
+
+
+    public static void printSecondpattern() {
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
     }
 }
