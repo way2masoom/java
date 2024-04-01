c35c53342b84964eab77367db7cf100f2ab97419
way2masoom <way2masoom@gmail.com>
2024-04-01 13:07:34 +0530

    Created a loop Chapter and add a example of Loop

---
+++ Lecture Code/5 While loop, Methods and Arrays/Lecture/src/Loop.java
@@ -0,0 +1,12 @@
+public class Loop {
+    public static void main(String[] args) {
+//        Example of Loop
+        System.out.println("Printing numbers form 1 to 100 using loop");
+
+        int num=1; // Initialization
+        while (num<=100){ // Condition
+            System.out.println(num); // Output of the code
+            num=num+1; // Updating the Condition
+        }
+    }
+}
