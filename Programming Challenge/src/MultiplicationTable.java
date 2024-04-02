1f0868332147740e1cf262175d30925cecfa9593
way2masoom <way2masoom@gmail.com>
2024-04-02 10:33:26 +0530

    Program to print MultiplicationTable

---
+++ Programming Challenge/src/MultiplicationTable.java
@@ -0,0 +1,23 @@
+//Develop a program that prints the multiplication table for a given number.
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        System.out.println("Printing the Table of Multiplication Table");
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int num=input.nextInt();
+
+        //calling method
+         printMultiplicationTable(num);
+    }
+
+    public static void printMultiplicationTable(int num){
+        int i=1;
+        while (i<=10){
+            System.out.println( num + " X " + i + " = " + (num * i));
+            i++;
+        }
+    }
+}
