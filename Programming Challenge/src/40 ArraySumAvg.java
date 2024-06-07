e8976a0e923b610c139f2fb8133831b15e16d7bc
way2masoom <way2masoom@gmail.com>
2024-06-07 15:53:14 +0545

    Array Sum & Aversge

--- Programming Challenge/src/40 ArraySumAvg.java
+++ Programming Challenge/src/40 ArraySumAvg.java
@@ -6,22 +6,34 @@ class SumandAvg {
      public static void main(String[] args) {
          System.out.println("Welcome to Sum and Average calculator");
 
-         Scanner input=new Scanner(System.in);
-         int[] arr={10,20,30,40};
-         int sum=ArraSum(arr);
+         // Calling the method to input the array from the user
+         int [] numArray=ArrayUtility.inputArray();
 
-         System.out.println(" Sum of array is = "+sum);
+         // Calculating the sum and average of the array elements
+         long sum=sum(numArray);
+         double avg=average(numArray);
 
-     }
-     public static int ArraSum(int[] arr){
-         int i=0;
-         int arrsum=0;
-         int avg=0;
-         while(i < arr.length){
-             arrsum = arrsum + arr[i];
-             i++;
-         }
-         return arrsum;
+         // Displaying the sum and average of the numbers
+         System.out.println("SUM of the number is = "+sum);
+         System.out.println("Average of the number is = "+avg);
      }
 
+    // Method to calculate the sum of the array elements
+    public static long sum(int[] numArray){
+            long sum=0;
+            int i=0;
+            while (i<numArray.length){
+                sum+=numArray[i];
+                i++;
+            }
+         return sum;
+     }
+
+    // Method to calculate the average of the array elements
+     public static double average(int[] numArray ){
+        long sum=sum(numArray);
+
+         return (double) (sum/numArray.length);
+     }
 }
+
--- Programming Challenge/src/ArrayUtility.java
+++ Programming Challenge/src/ArrayUtility.java
@@ -1,2 +1,22 @@
-class ArrayUtility {
+import java.util.Scanner;
+
+public class ArrayUtility {
+
+    // Method to input array from the user
+    public static int[] inputArray(){
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Enter the Size of Array: ");
+        int size=input.nextInt();
+
+        int[] nums=new int[size];
+        // Taking array elements input from the user
+        int i=0;
+        while (i<size){
+            System.out.println("Please Enter Element No"+(i+1)+" ");
+            nums[i]=input.nextInt();
+            i++;
+        }
+        return nums;
+    }
 }
